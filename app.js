719dd18d0e792442d2753056602e08141eccd34a
shirshir05 <shir0@post.bgu.ac.il>
2020-04-24 11:27:17 +0300

    add character

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var context;
 var shape = new Object();
+var character = new Object();
 var board;
 var score;
 var pac_color;
+var character_color;
 var start_time;
 var time_elapsed;
 var interval;
+
 
 $(document).ready(function() {
 	context = canvas.getContext("2d");
@@ -16,6 +19,7 @@ function Start() {
 	board = new Array();
 	score = 0;
 	pac_color = "yellow";
+	character_color = "pink"
 	var cnt = 100;
 	var food_remain = 50;
 	var pacman_remain = 1;
@@ -24,26 +28,31 @@ function Start() {
 		board[i] = new Array();
 		//put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
 		for (var j = 0; j < 10; j++) {
-			if (
+			if(i == 0 && j == 0){
+				character.i = i;
+				character.j = j;
+				board[i][j] = 5;
+			}
+			else if (
 				(i == 3 && j == 3) ||
 				(i == 3 && j == 4) ||
 				(i == 3 && j == 5) ||
 				(i == 6 && j == 1) ||
 				(i == 6 && j == 2)
 			) {
-				board[i][j] = 4;
+				board[i][j] = 4;//block
 			} else {
 				var randomNum = Math.random();
 				if (randomNum <= (1.0 * food_remain) / cnt) {
 					food_remain--;
-					board[i][j] = 1;
+					board[i][j] = 1;//food
 				} else if (randomNum < (1.0 * (pacman_remain + food_remain)) / cnt) {
 					shape.i = i;
 					shape.j = j;
-					pacman_remain--;
+					pacman_remain--;//pacamne
 					board[i][j] = 2;
 				} else {
-					board[i][j] = 0;
+					board[i][j] = 0;//blank
 				}
 				cnt--;
 			}
@@ -69,7 +78,7 @@ function Start() {
 		},
 		false
 	);
-	interval = setInterval(UpdatePosition, 250);
+	interval = setInterval(UpdatePosition, 70);
 }
 
 function findRandomEmptyCell(board) {
@@ -126,36 +135,104 @@ function Draw() {
 				context.rect(center.x - 30, center.y - 30, 60, 60);
 				context.fillStyle = "grey"; //color
 				context.fill();
+			}else if (board[i][j] == 5) {
+				context.beginPath();
+				context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
+				context.lineTo(center.x, center.y);
+				context.fillStyle = character_color; //color
+				context.fill();
+				context.beginPath();
 			}
 		}
 	}
 }
 
+
+function changPositionCharacter(){
+	board[character.i][character.j] = 0;
+	//0 - 
+	let found = false;
+	while (found == false){
+		let move = Math.floor(Math.random() * Math.floor(4));
+		if (move == 0) {
+			if (checkMoveLeft()) {
+				character.j--;
+				found = true;
+			}
+		}else if (move == 1) {
+			if (checkMoveRight()) {
+				character.j++;
+				found = true;
+			}
+		}else if (move == 2) {
+			if (checkMoveDown()) {
+				character.i--;
+				found = true;
+			}
+		} else if (move == 3) {
+			if (checkMoveUp()) {
+				character.i++;
+				found = true;
+			}
+		}
+	}
+	board[character.i][character.j] = 5;
+}
+
+function checkMoveLeft(){
+	return character.j > 0 && board[character.i][character.j - 1] != 4;
+
+}
+
+function checkMoveRight(){
+	return character.j < 9 && board[character.i][character.j + 1] != 4;
+}
+
+function checkMoveDown(){
+	return character.i > 0 && board[character.i - 1][character.j] != 4;
+
+}
+
+function checkMoveUp(){
+	return character.i < 9 && board[character.i + 1][character.j] != 4;
+
+}
+
+
 function UpdatePosition() {
 	board[shape.i][shape.j] = 0;
 	var x = GetKeyPressed();
+	//i -x
+	//j - y
 	if (x == 1) {
 		if (shape.j > 0 && board[shape.i][shape.j - 1] != 4) {
 			shape.j--;
+			changPositionCharacter()
 		}
 	}
 	if (x == 2) {
 		if (shape.j < 9 && board[shape.i][shape.j + 1] != 4) {
 			shape.j++;
+			changPositionCharacter()
 		}
 	}
 	if (x == 3) {
 		if (shape.i > 0 && board[shape.i - 1][shape.j] != 4) {
 			shape.i--;
+			changPositionCharacter()
 		}
 	}
 	if (x == 4) {
 		if (shape.i < 9 && board[shape.i + 1][shape.j] != 4) {
 			shape.i++;
+			changPositionCharacter()
 		}
 	}
 	if (board[shape.i][shape.j] == 1) {
 		score++;
+	}
+	if (board[shape.i][shape.j] == 5) {
+		score += 50;
 	}
 	board[shape.i][shape.j] = 2;
 	var currentTime = new Date();
@@ -164,7 +241,7 @@ function UpdatePosition() {
 		pac_color = "green";
 	}
 	if (score == 50) {
-		window.clearInterval(interval);
+		window.clearInterval(interval);		
 		window.alert("Game completed");
 	} else {
 		Draw();
